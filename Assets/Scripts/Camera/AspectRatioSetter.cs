6363cde633886f0495bb5361b4b75d3bd901d819
Charamel495 <to_1525@yahoo.co.jp>
2026-06-04 21:17:52 +0900

    キャンバス修正

---
+++ Assets/Scripts/Camera/AspectRatioSetter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace KurokumaSoft
+{
+
+    public sealed class AspectRatioSetter : MonoBehaviour
+    {
+
+        [SerializeField]
+        Camera targetCamera;
+        [SerializeField]
+        Vector2 targetSize = new(16f, 9f);
+        [SerializeField]
+        bool autoSetOnStart = true;
+
+        public Camera TargetCamera { get { return targetCamera; } set { targetCamera = value; } }
+        public Vector2 TargetSize { get { return targetSize; } set { targetSize = value; } }
+
+        void Awake()
+        {
+            if (autoSetOnStart)
+            {
+                SetAspectRatio();
+            }
+        }
+
+        public void SetAspectRatio()
+        {
+            if (targetCamera == null)
+            {
+                return;
+            }
+
+            float currentRatio = (float)Screen.width / Screen.height;
+            float targetRatio = targetSize.x / targetSize.y;
+            float scaleHeight = currentRatio / targetRatio;
+            Rect rect = targetCamera.rect;
+
+            if (scaleHeight < 1f)
+            {
+                rect.width = 1f;
+                rect.height = scaleHeight;
+                rect.x = 0;
+                rect.y = (1f - scaleHeight) / 2f;
+            }
+            else
+            {
+                float scaleWidth = 1f / scaleHeight;
+                rect.width = scaleWidth;
+                rect.height = 1f;
+                rect.x = (1f - scaleWidth) / 2f;
+                rect.y = 0;
+            }
+
+            targetCamera.rect = rect;
+        }
+
+    }
+
+}
